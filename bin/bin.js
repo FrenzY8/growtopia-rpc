a4e4647857873fb7b18e4098f15b6a34446225bf
Frenzy Singapore <75056113+FrenzY8@users.noreply.github.com>
2021-08-31 02:43:02 +0000

    Update bin.js

--- bin/bin.js
+++ bin/bin.js
@@ -1,12 +1,26 @@
+// Assalammualaikum Warahmatullahiwabarokatuh
+/*Coded by FrenzySG#9999
+Thanks to : 
+- iMix
+- Goodless
+- iCrawl
+- ChickenGTPS*/
+
+// importing path
 #!/usr/bin/env node
 
 var join = require('path').join
 var childProcess = require('child_process');
 var args = process.argv.slice(2);
 
+/* Failed Imported
+const fs = require('fs')
+const api = require('api.frenzy.xyz')
+Stfu its Just Failed */
+
  args.unshift(__dirname + '/../'); 
 
 childProcess.exec('npm start', (err, stdout) => {
-if (err) console.log(err);
+if (err) console.log(err); // restart like usual
 console.log(stdout);
 })
